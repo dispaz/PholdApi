ceef02c13410fd8592b38981187d848d3d24f47a
dispaz <vladvoloshyn2@gmail.com>
2020-02-29 12:26:29 +0100

    Phold Api empty project

---
+++ PholdApi/Controllers/PholdController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PholdApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PholdController : ControllerBase
+    {
+        private readonly ILogger<PholdController> _logger;
+
+        public PholdController(ILogger<PholdController> logger)
+        {
+            _logger = logger;
+        }
+
+       
+    }
+}
