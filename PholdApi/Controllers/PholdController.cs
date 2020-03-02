258878bb9107fe4606d8505deff5ff1978e7ef41
dispaz <vladvoloshyn2@gmail.com>
2020-03-02 20:22:43 +0100

    Get image url from storage account using S A S

--- PholdApi/Controllers/PholdController.cs
+++ PholdApi/Controllers/PholdController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.File;
 
 namespace PholdApi.Controllers
 {
@@ -12,17 +15,47 @@ namespace PholdApi.Controllers
     public class PholdController : ControllerBase
     {
         private readonly ILogger<PholdController> _logger;
+        private readonly IConfiguration _config;
 
-        public PholdController(ILogger<PholdController> logger)
+        public PholdController(ILogger<PholdController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
         
         [HttpGet]
         [Route("Get")]
-        public string GetTest()
+        public async Task<ActionResult<string>> GetTestAsync()
         {
-            return "test";
+            var _connString = _config.GetSection("StorageAccountConnectionString").Value;
+            CloudStorageAccount storageAccount;
+            if(!CloudStorageAccount.TryParse(_connString, out storageAccount))
+            {
+                return StatusCode(500, "Unable to connect to storage");
+            }
+            var fileClient = storageAccount.CreateCloudFileClient();
+            var share = fileClient.GetShareReference("phold");
+            string policyName = "PholdSharePolicy" + DateTime.UtcNow.Ticks;
+            var sharedPolicy = new SharedAccessFilePolicy()
+            {
+                SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(1),
+                Permissions = SharedAccessFilePermissions.Read
+            };
+
+            var permissions = await share.GetPermissionsAsync();
+
+            permissions.SharedAccessPolicies.Add(policyName, sharedPolicy);
+            await share.SetPermissionsAsync(permissions);
+
+            
+            var rootDir = share.GetRootDirectoryReference();
+            var sampleDir = rootDir.GetDirectoryReference("photos");
+            var file = sampleDir.GetFileReference("1.jpg");
+
+            var sasToken = file.GetSharedAccessSignature(null, policyName);
+            var fileSasUri = new Uri(file.StorageUri.PrimaryUri.ToString() + sasToken);
+
+            return Ok(fileSasUri);
         }
        
     }
