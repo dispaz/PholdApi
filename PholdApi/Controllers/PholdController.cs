de6cd0b59a410a5bf4e2ec6c143d3da978862860
dispaz <vladvoloshyn2@gmail.com>
2020-03-28 21:36:13 +0100

    Revert "Create or Update Phold Object"
    This reverts commit e7108e91ef4627892c0dc8bd28a8bd47ae5e827d.

--- PholdApi/Controllers/PholdController.cs
+++ PholdApi/Controllers/PholdController.cs
@@ -10,13 +10,9 @@ using Microsoft.WindowsAzure.Storage.File;
 using System.IO;
 using Microsoft.WindowsAzure.Storage.Blob;
 using PholdApi.Interfaces;
-using Microsoft.AspNetCore.Http;
-using PholdApi.Filters;
-using PholdApi.Models;
 
 namespace PholdApi.Controllers
 {
-    [ServiceFilter(typeof(CredentialsFilter))]
     [ApiController]
     [Route("[controller]")]
     public class PholdController : ControllerBase
@@ -24,51 +20,26 @@ namespace PholdApi.Controllers
         private readonly ILogger<PholdController> _logger;
         private readonly IConfiguration _config;
         private readonly IPholdStorageService _storageService;
-        private readonly IDbService _dbService;
 
-        public PholdController(ILogger<PholdController> logger, IConfiguration config, IPholdStorageService storageService, IDbService dbService)
+        public PholdController(ILogger<PholdController> logger, IConfiguration config, IPholdStorageService storageService)
         {
             _logger = logger;
             _config = config;
             _storageService = storageService;
-            _dbService = dbService;
         }
 
         /// <summary>
         /// Get urls to download images
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="ids"></param>
         /// <returns>List of urls to images</returns>
-        [ProducesResponseType(200)]
-        [ProducesResponseType(404)]
-        [ProducesResponseType(500)]
         [HttpGet]
-        [Route("ObjectImagesById")]
-        public async Task<ActionResult<List<Uri>>> GetUrlsForObjectImageByIdsAsync(int id)
+        [Route("ImagesByIds")]
+        public ActionResult<List<Uri>> GetUrlsForObjectImageByIds([FromHeader] int [] ids)
         {
             //TODO logging
-            try
-            {
-                var images = await _storageService.GetImagesAsync(id);
-
-                if (images.Count > 0)
-                    return Ok(images);
-                else
-                    return StatusCode(404, "Not found images for that id");
-
-            }
-            catch(Exception e)
-            {
-                return StatusCode(500, e.Message);
-            }
+            return Ok(_storageService.GetImages(ids));            
         }
 
-        [HttpPost]
-        [Route("CreateNewObject")]
-        public ActionResult<int> LoadImage(PholdObject pholdObject, [FromForm] IFormFile[] image)
-        {
-            //_dbService.AddOrUpdatePholdObject()   
-            return StatusCodes.Status200OK;
-        }
     }
 }
--- PholdApi/Startup.cs
+++ PholdApi/Startup.cs
@@ -1,10 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using PholdApi.Filters;
+using Microsoft.Extensions.Logging;
 using PholdApi.Interfaces;
 using PholdApi.Services;
 
@@ -25,41 +30,23 @@ namespace PholdApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddScoped<CredentialsFilter>();
             services.AddScoped<IPholdStorageService, PholdStorageService>();
-            services.AddScoped<IDbService, DbService>();
-            services.AddScoped<ICredentialsService, CredentialsService>();
-            
-            services.AddSwaggerGen(c =>
+            services.AddOpenApiDocument(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = _docName, Version = "v1" });
-                c.OperationFilter<SwaggerImageOperationFilter>();
-
-                c.AddSecurityDefinition("api-key", new OpenApiSecurityScheme
-                {
-                    Type = SecuritySchemeType.ApiKey,
-                    Name = "api-key",
-                    Description = "api-key",
-                    In = ParameterLocation.Query
-                });
-
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "api-key"}
-                        },
-                        new [] {""}
-                    }
-                });
+                c.Title = _docName;
+                c.Version = "V1";
             });
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseOpenApi();
             app.UseHttpsRedirection();
             app.UseRouting();
 
@@ -69,17 +56,7 @@ namespace PholdApi
             {
                 endpoints.MapControllers();
             });
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            app.UseSwagger();
-            app.UseSwaggerUI(options =>
-            {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", _docName);
-            });
-            
+            app.UseSwaggerUi3();
         }
     }
 }
