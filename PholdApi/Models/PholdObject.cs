1bb81479ecc600882bfcc105afe355035f7e29c4
dispaz <vladvoloshyn2@gmail.com>
2020-04-22 18:39:50 +0200

    ImageUrls only for get requests

--- PholdApi/Models/PholdObject.cs
+++ PholdApi/Models/PholdObject.cs
@@ -12,15 +12,15 @@ namespace PholdApi.Models
         public double Latitude { get; set; }
         public double Longitude { get; set; }
         public string Description { get; set; }
-        public string AreaCode { get; set; }
-        public List<string> ImageUrls { get; set; }
+        public string AreaCode { get; set; }        
 
     }
 
     public class PholdObject : BasePholdObject
     {
         public int ID { get; set; }
-        
+        public List<string> ImageUrls { get; set; }
+
     }
 
     public class SavePholdObject : BasePholdObject
